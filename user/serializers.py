e2de9ab318a65e94dc7b2b5e05e94bb65ab05de0
Yaroslav Sysoiev <mrsomeonepromax@gmail.com>
2025-01-29 12:11:28 +0200

    add field 'username' to userserializer

--- user/serializers.py
+++ user/serializers.py
@@ -5,7 +5,7 @@ from rest_framework import serializers
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = get_user_model()
-        fields = ("id", "email", "password", "is_staff")
+        fields = ("id", "username", "email", "password", "is_staff")
         read_only_fields = (
             "id",
             "is_staff",
@@ -13,11 +13,11 @@ class UserSerializer(serializers.ModelSerializer):
         extra_kwargs = {"password": {"write_only": True, "min_length": 5}}
 
     def create(self, validated_data):
-        """Create a new user with encrypted password and return it"""
+        """Create User with encrypted password"""
         return get_user_model().objects.create_user(**validated_data)
 
     def update(self, instance, validated_data):
-        """Update a user, set the password correctly and return it"""
+        """Update User with encrypted password"""
         password = validated_data.pop("password", None)
         user = super().update(instance, validated_data)
         if password:
