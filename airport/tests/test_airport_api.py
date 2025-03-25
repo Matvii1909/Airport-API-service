a6bc9d361f4af05c355bb986d2d29ddb6be903f8
Yaroslav Sysoiev <mrsomeonepromax@gmail.com>
2025-03-25 19:19:45 +0200

    Update test: filter test added

--- airport/tests/test_airport_api.py
+++ airport/tests/test_airport_api.py
@@ -5,8 +5,9 @@ from django.urls import reverse
 from rest_framework.test import APIClient
 from rest_framework import status
 
-from airport.models import Airport, Route, Airplane, Flight, Crew
-from airport.serializers import AirportSerializer, RouteSerializer, AirplaneSerializer, FlightSerializer
+from airport.models import Airport, Route, Airplane, Flight, Crew, Type
+from airport.serializers import AirportSerializer, RouteSerializer, AirplaneSerializer, FlightSerializer, \
+    AirplaneListSerializer
 
 AIRPORT_URL = reverse("airport:airport-list")
 ROUTE_URL = reverse("airport:route-list")
@@ -42,6 +43,7 @@ def sample_airplane(**params):
         "seats_in_row": 6,
     }
     defaults.update(params)
+
     return Airplane.objects.create(**defaults)
 
 
@@ -76,17 +78,48 @@ class AuthenticatedAirportApiTests(TestCase):
         )
         self.client.force_authenticate(self.user)
 
-    def test_list_airports(self):
-        sample_airport()
-        sample_airport()
+    def test_airplanes_list(self):
+        sample_airplane()
+        airplane_with_airplane_types = sample_airplane()
 
-        res = self.client.get(AIRPORT_URL)
+        airplane_type_1 = Type.objects.create(name="Magic Aircraft")
+        airplane_type_2 = Type.objects.create(name="Plastic Aircraft")
 
-        airports = Airport.objects.order_by("id")
-        serializer = AirportSerializer(airports, many=True)
+        airplane_with_airplane_types.types.add(airplane_type_1, airplane_type_2)
+
+        res = self.client.get(AIRPLANE_URL)
+
+        airplanes = Airplane.objects.all()
+        serializer = AirplaneListSerializer(airplanes, many=True)
 
         self.assertEqual(res.status_code, status.HTTP_200_OK)
         self.assertEqual(res.data, serializer.data)
+
+    def test_filter_airplanes_by_types(self):
+        airplane_type_1 = Type.objects.create(name="Aircraft X")
+        airplane_type_2 = Type.objects.create(name="Aircraft Y")
+
+        airplane_1 = sample_airplane(name="Aircraft 10-57")
+        airplane_2 = sample_airplane(name="Aircraft 10-58")
+
+        airplane_1.types.add(airplane_type_1)
+        airplane_2.types.add(airplane_type_2)
+
+        airplane_without_type = sample_airplane(name="Airplane without type")
+        print(airplane_without_type.types)
+
+        res = self.client.get(
+            AIRPLANE_URL,
+            {"types": f"{airplane_type_1.name},{airplane_type_2.name}"},
+        )
+
+        serializer_without_aircraft_type = AirplaneListSerializer(airplane_without_type)
+        serializer_aircraft_type_1 = AirplaneListSerializer(airplane_1)
+        serializer_aircraft_type_2 = AirplaneListSerializer(airplane_2)
+
+        self.assertIn(serializer_aircraft_type_1.data, res.data)
+        self.assertIn(serializer_aircraft_type_2.data, res.data)
+        self.assertNotIn(serializer_without_aircraft_type.data, res.data)
 
     def test_create_airport_forbidden(self):
         payload = {"name": "New Airport", "closest_big_city": "New City"}
