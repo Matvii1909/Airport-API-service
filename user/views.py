cd8d9698367a9f0c06c623a57d6b1f540539648d
Yaroslav Sysoiev <mrsomeonepromax@gmail.com>
2025-04-14 18:18:32 +0300

    Update user app: remove comments&redundant lines

--- user/views.py
+++ user/views.py
@@ -1,11 +1,10 @@
 from rest_framework import generics
-from rest_framework.authentication import TokenAuthentication
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.authtoken.views import ObtainAuthToken
 from rest_framework.settings import api_settings
 from rest_framework_simplejwt.authentication import JWTAuthentication
 
-from .serializers import UserSerializer, AuthTokenSerializer
+from user.serializers import UserSerializer, AuthTokenSerializer
 
 
 class CreateUserView(generics.CreateAPIView):
